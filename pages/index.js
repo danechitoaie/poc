3d7430220d7f3c7cdea9582bedaea95dfeb4385f
Daniel Anechitoaie <10779010+danechitoaie@users.noreply.github.com>
2021-02-26 17:11:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Index() {
     return (
         <div>
             <ul>
-                <li><a href="/poc1">POC 1</a></li>
+                <li><a href="/poc1">POC 1 Test</a></li>
                 <li><a href="/poc2">POC 2</a></li>
             </ul>
         </div>
